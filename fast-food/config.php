d1d0ea8b39e4102b48286ef08d855b1f936b66fa
LEMINHTHI-CM24V7X628 <leminhthi1997@gmail.com>
2025-11-05 21:34:32 +0700

    Update config.php

--- fast-food/config.php
+++ fast-food/config.php
@@ -1,21 +1,4 @@
-<?php
-if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
-if (!defined('DB_PORT')) define('DB_PORT', '5432');
-if (!defined('DB_NAME')) define('DB_NAME', 'db_fastfood');
-if (!defined('DB_USER')) define('DB_USER', 'postgres');
-if (!defined('DB_PASS')) define('DB_PASS', '12345');
 
-try {
-    $pdo = new PDO(
-        "pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME,
-        DB_USER,
-        DB_PASS
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("<h3 style='color:red'>Kết nối thất bại: " . htmlspecialchars($e->getMessage()) . "</h3>");
-}
-?>
 <?php
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
